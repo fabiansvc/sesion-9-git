0d6e1590234571c889feae0763473dede26364ff
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2024-10-10 16:53:14 -0500

    changed

--- src/main/java/com/example/sesion9git/HelloApplication.java
+++ src/main/java/com/example/sesion9git/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 200);
         stage.setTitle("Second commit");
-        stage.setResizable(false);
+        stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
